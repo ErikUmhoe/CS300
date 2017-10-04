a508e2967b5684730723e8d4ba68293d980b3f8a
Nick Stoffel <nickstoffel@ymail.com>
2017-10-03 22:33:37 -0500

    Read ssd
    Added ssd

--- SwimSimulation.java
+++ SwimSimulation.java
@@ -4,90 +4,73 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class SwimSimulation {
-
 	private PApplet processing;
-	private Fish[] fishes;
-	private Food[] foods;
+	private Fish[] fish;
+	private Food[] food;
 	private Hook hook;
-
-	public static void main(String[] args) {
-		
-		
-	}
-
-	public SwimSimulation(PApplet processing) {
-
+	
+	public SwimSimulation(PApplet processing)
+	{
 		this.processing = processing;
+		
 		File ssf = new File("FileOptions.ssf");
 		String[] ssdLocations = readSSF(ssf);
 		
-		if(ssdLocations[0]==null)
+		for(int i = 0; i < ssdLocations.length; i++)
 		{
-			this.loadDefault(processing);
-		}
-		else {
-			for(int i = 0; i < ssdLocations.length; i++)
-			{
-				System.out.println(ssdLocations[i]);
-			}
+			System.out.println(ssdLocations[i]);
 		}
 		
-		/*int height = processing.height, width = processing.width;
-		int fishNum = 4, foodNum = 6, hookNum = 1;
+		int randomSSD = Utility.randomInt(ssdLocations.length);
+		System.out.println("Random location: " + ssdLocations[randomSSD]);
 		
-		fishes = new Fish[fishNum];
-		hook = new Hook(processing);
-		foods = new Food[foodNum];
+		readSSD(ssdLocations[randomSSD]);
 		
-		for (int i = 0; i < fishNum; i++) {
-			fishes[i] = new Fish(this.processing);
-		}
-		for (int i = 0; i < foodNum; i++) {
-			foods[i] = new Food(this.processing);
-		}*/
-
+		//fish = new Fish[4];
+		//for(int num = 0; num < fish.length; num++)
+			//fish[num] = new Fish(processing);
+		
+		//food = new Food[6];
+		//for(int num = 0; num < food.length; num++)
+		//	food[num] = new Food(processing);
+		
+		//hook = new Hook(processing);
 	}
-
-	public void update() {
+	
+	public void update()
+	{
 
 		processing.clear();
-		processing.background(0, 255, 255);
-
-		/*
-		 * for(int row = 0; row < fishPositions.length; row++) {
-		 * Main.placeObjectInTank("><(('>",processing, fishPositions[row][0],
-		 * fishPositions[row][1]); }
-		 */
-		/*
-		 * for(int row = 0; row < foodPositions.length; row++) {
-		 * Main.placeObjectInTank("*",processing, foodPositions[row][0],
-		 * foodPositions[row][1]); } for(int row = 0; row < hookPositions.length; row++)
-		 * { Main.placeObjectInTank("J",processing,
-		 * hookPositions[row][0],hookPositions[row][1]); }
-		 */
-		for (int i = 0; i < fishes.length; i++) {
-			fishes[i].update();
-			for (int x = 0; x < foods.length; x++) {
-				fishes[i].tryToEat(foods[x]);
-			}
-			hook.tryToCatch(fishes[i]);
-		}
-		for (int i = 0; i < foods.length; i++) {
-			foods[i].update();
-		}
+		processing.background(0,255,255);
+		
+		for(int num = 0; num < fish.length; num++)
+			fish[num].update();
+		
+		for(int num = 0; num < food.length; num++)
+			food[num].update();
+		
 		hook.update();
-
+		
+		for(int numFish = 0; numFish < fish.length; numFish++)
+		{
+			for(int numFood = 0; numFood < food.length; numFood++)
+			{
+				fish[numFish].tryToEat(food[numFood]);
+			}
+		}
+		
+		for(int numFish = 0; numFish < fish.length; numFish++)
+		{
+			hook.tryToCatch(fish[numFish]);
+		}
 	}
-
-	public void handleClick(int mouseX, int mouseY) {
-		/*
-		 * hookPositions[0][0] = mouseX; hookPositions[0][1] = processing.height-1;
-		 */
-
+	
+	public void handleClick(int mouseX, int mouseY)
+	{
 		hook.handleClick(mouseX, mouseY);
 	}
-
-	public static String[] readSSF(File ssf)
+	
+	public String[] readSSF(File ssf)
 	{
 		String[] files;
 		String sub = "";
@@ -118,24 +101,81 @@ public class SwimSimulation {
 		return files;
 		
 	}
-	
-	private void loadDefault(PApplet processing)
+	private void readSSD(String ssdLoc)
 	{
-		fishes = new Fish[4];
-		foods = new Food[6];
-		hook = new Hook(this.processing);
-		for(int i = 0; i < fishes.length; i++)
-		{
-			fishes[i] = new Fish(processing);
-		}
-		for(int i = 0; i < foods.length; i++)
-		{
-			foods[i] = new Food(processing);
-		}	
+		Scanner reader = null;
+		String[] sub;
+		String[] objectPosition;
+		ArrayList<String> lines = new ArrayList<String>();
+		File ssd = new File(ssdLoc);
+		int index;
 		
+		try {
+			reader = new Scanner(ssd);
+			while(reader.hasNextLine())
+			{
+				lines.add(reader.nextLine());
+			}
+			for(int x = 0; x < lines.size(); x++)
+			{
+				lines.get(x).trim();
+				lines.set(x, lines.get(x).toLowerCase());
+				if(lines.get(x).isEmpty())
+				{
+					lines.remove(x);
+					x--;
+				}
+			}
+			
+			for(int x = 0; x < lines.size(); x++)
+		    {
+				System.out.println("Line " + x + ": "+lines.get(x));
+		    }
+			
+			for(int x = 0; x < lines.size(); x++)
+			{
+				if(lines.get(x).contains("fish"))
+				{
+					sub = lines.get(x).split(":");
+					sub[1] = sub[1].trim();
+					index = Integer.parseInt(sub[1]);
+					fish = new Fish[index];
+					for(int y = 1; y <= index; y++)
+					{
+						objectPosition = lines.get( x + y ).split(",");
+						fish[y - 1] = new Fish(processing, Integer.parseInt(objectPosition[0].trim()), Integer.parseInt(objectPosition[1].trim()));
+					}
+					x += index;
+				}
+				else if(lines.get(x).contains("food"))
+				{
+					sub = lines.get(x).split(":");
+					sub[1] = sub[1].trim();
+					index = Integer.parseInt(sub[1]);
+					food = new Food[index];
+					for(int y = 1; y <= index; y++)
+					{
+						objectPosition = lines.get( x + y ).split(",");
+						food[y - 1] = new Food(processing, Integer.parseInt(objectPosition[0].trim()), Integer.parseInt(objectPosition[1].trim()));
+					}
+					x += index;
+				}
+				else if(lines.get(x).contains("hook"))
+				{
+					objectPosition = lines.get( x + 1 ).split(",");
+					hook = new Hook(processing, Integer.parseInt(objectPosition[0].trim()), Integer.parseInt(objectPosition[1].trim()));
+				}
+				else
+				{
+					
+				}
+			}
+			
+		}catch(FileNotFoundException e)
+		{
+			System.out.println("WARNING: Could not find or open the " + ssdLoc + " file.");
+		}finally {
+            if( reader != null ) reader.close();
+        }
 	}
-	
-	
-	
-
 }
