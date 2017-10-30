7d3286d978f4b124474103ae589fcb1958dfce06
ErikUmhoe <98eriku@gmail.com>
2017-10-30 15:39:38 -0500

    Final code dump for this i believe

--- eclipse-workspace/Generating Philosophy/src/Main.java
+++ eclipse-workspace/Generating Philosophy/src/Main.java
@@ -6,8 +6,28 @@ public class Main {
 	public static void main(String[] args) {
 		String wikiChoice;
 		Scanner input = new Scanner(System.in);
-		
 		System.out.println("Please enter a Wikipedia topic:" );
+		wikiChoice = input.nextLine();
+		wikiChoice = wikiChoice.replaceAll(" ", "_");
+		wikiChoice = wikiChoice.trim();
+		System.out.println(wikiChoice);
+
+		int count = 0;
+		boolean found = false;
+		for(String i : new Generator<String>(100,"/wiki/"+wikiChoice,new NetWikiLinkFunction()))
+		{
+			System.out.println(count +": " + i);
+			count++;
+			if(i.indexOf("Philosophy") > 0)
+			{	
+				found = true;
+				break;
+			}
+			else if(i.contains("FAILED"))
+				break;
+				
+		}
+		
 	}
 
 }
